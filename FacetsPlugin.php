cc7cfb8ab19a596f6ec159caa7188c645e0bf174
DBinaghi <39097246+DBinaghi@users.noreply.github.com>
2021-06-08 10:51:56 +0200

    Update files, add config_form

--- FacetsPlugin.php
+++ FacetsPlugin.php
@@ -1,125 +1,193 @@
 <?php
 /**
  * @copyright Jean-Baptiste HEREN, 2018
+ * @copyright Daniele BINAGHI, 2021
  * @license http://www.gnu.org/licenses/gpl-3.0.txt
  * @package FacetsPlugin
  */
 
 if (!defined('FACETS_PLUGIN_DIR')) {
-    define('FACETS_PLUGIN_DIR', dirname(__FILE__));
+	define('FACETS_PLUGIN_DIR', dirname(__FILE__));
 }
+define('FACETS_MINIMUM_AMOUNT', 10);
 
 require_once FACETS_PLUGIN_DIR . '/helpers/FacetsFunctions.php';
 
 class FacetsPlugin extends Omeka_Plugin_AbstractPlugin
 {
+	protected $_hooks = array(
+		'install',
+		'uninstall',
+		'initialize',
+        'config_form',
+        'config',
+		'define_routes',
+		'public_head',
+		'public_items_browse',
+		'public_items_facets'
+	);
 
-    protected $_hooks = array(
-        'install',
-        'uninstall',
-        'initialize',
-        'define_routes',
-        'public_head',
-        'items_browse_sql',
-        'public_items_browse'
-    );
+	/**
+	 * Installs the plugin.
+	 */
+	public function hookInstall()
+	{
+		set_option('facets_public_hook', 'public_items_browse');
+        set_option('facets_description', '');
+        set_option('facets_hide_single_entries', 0);
+        set_option('facets_sort_order', 'count_alpha');
 
-    /**
-     * Installs the plugin.
-     */
-    public function hookInstall()
-    {
-    }
-
-    /**
-     * Uninstalls the plugin.
-     */
-    public function hookUninstall()
-    {
-    }
-
-    /**
-     * Initialize this plugin.
-     */
-    public function hookInitialize()
-    {
-      get_view()->addHelperPath(dirname(__FILE__) . '/views/helpers', 'Facets_View_Helper_');
-    }
-
-    public function hookPublicHead($args)
-    {
-      queue_css_file('facets');
-    }
-
-    public function hookDefineRoutes($args)
-    {
-        if (is_admin_theme()) {
-            return;
-        }
-
-        //Itemtypes browse
-        $router = $args['router'];
-        $router->addRoute(
-        		'facets',
-        		new Zend_Controller_Router_Route(
-        				"facets",
-        				array(
-        						'module'    => 'facets',
-        						'controller' => 'facets',
-        						'action'     => 'browse'
-        				)
-        		)
+		$defaults = array(
+            'item_elements' => array()
         );
+        set_option('facets_elements', json_encode($defaults));
+        set_option('facets_item_types', 1);
+        set_option('facets_collections', 1);
+        set_option('facets_tags', 0);
+	}
 
+	/**
+	 * Uninstalls the plugin.
+	 */
+	public function hookUninstall()
+	{
+		delete_option('facets_public_hook');
+        delete_option('facets_description');
+        delete_option('facets_hide_single_entries');
+        delete_option('facets_sort_order');
+        delete_option('facets_elements');
+        delete_option('facets_item_types');
+        delete_option('facets_collections');
+        delete_option('facets_tags');
+	}
+
+	/**
+	 * Initialize this plugin.
+	 */
+	public function hookInitialize()
+	{
+		get_view()->addHelperPath(dirname(__FILE__) . '/views/helpers', 'Facets_View_Helper_');
+
+        $settings = json_decode(get_option('facets_elements'), true);
+        $this->_settings = $settings;
+	}
+
+    /**
+     * Shows plugin configuration page.
+     */
+    public function hookConfigForm($args)
+    {
+        $settings = $this->_settings;
+
+        $table = get_db()->getTable('Element');
+        $select = $table->getSelect()
+			->where('element_sets.name = \'Dublin Core\'')
+            ->order('elements.element_set_id')
+            ->order('ISNULL(elements.order)')
+            ->order('elements.order');
+        $elements = $table->fetchObjects($select);
+
+        include('config_form.php');
     }
 
     /**
-     * Deal with search terms (advanced search).
-     *
-     * Allows to get refinements when a base Dublin Core is used.
-     *
-     * @internal This hook is used twice, first to prepare select for the
-     * current page, second to get the total count.
-     *
-     * @internal This should be used, because the filter is not enough for
-     * requests with a refinable element that should contains something.
-     *
-     * @internal MySql 5.5 has a limit: group by is done before order by, and it
-     * is complex to skirt.
-     *
-     * @see DublinCoreExtendedPlugin::filterItemsBrowseParams()
-     *
-     * @param Omeka_Db_Select $select
-     * @param array $params
+     * Handle the config form.
      */
-    public function hookItemsBrowseSql($args)
+    public function hookConfig($args)
     {
-        $db = $this->_db;
-        $select = $args['select'];
-        $params = $args['params'];
+        $post = $args['post'];
+		
+        set_option('facets_public_hook', $post['facets_public_hook']);
+        set_option('facets_description', $post['facets_description']);
+        set_option('facets_hide_single_entries', $post['facets_hide_single_entries']);
+        set_option('facets_sort_order', $post['facets_sort_order']);
 
-    }
+		$settings = array(
+            'item_elements' => isset($post['item_elements']) ? $post['item_elements'] : array()
+        );
+        set_option('facets_elements', json_encode($settings));
 
-    public function hookPublicItemsBrowse($args)
-    {
-      $view = $args['view'];
-      $items = $args['items'];
+        set_option('facets_item_types', $post['facets_item_types']);
+        set_option('facets_collections', $post['facets_collections']);
+        set_option('facets_tags', $post['facets_tags']);
+   }
 
-      // $params = $args['params'];
-      $itemsArray = array();
-      $allItems = get_db()->getTable('Item')->findAll();
-      // $allItems = $this->_helper->db->findBy($params);
-      foreach($allItems as $item){
-        $itemsArray[] = $item->id;
-      }
+	public function hookPublicHead($args)
+	{
+		queue_css_file('facets');
+	}
 
-      // echo get_view()->partial('facets/browse.php',array(
-      //     'itemsArray' => $itemsArray,
-      //     'items' => $items
-      //   ));
+	public function hookDefineRoutes($args)
+	{
+		if (is_admin_theme()) {
+			return;
+		}
 
-      echo get_view()->partial('facets/browse.php',array(
-          'itemsArray' => $itemsArray
-        ));
-    }
+		//Itemtypes browse
+		$router = $args['router'];
+		$router->addRoute(
+			'facets',
+			new Zend_Controller_Router_Route (
+				"facets",
+				array(
+					'module'	 => 'facets',
+					'controller' => 'facets',
+					'action'	 => 'browse'
+				)
+			)
+		);
+	}
+
+	public function hookPublicItemsBrowse($args)
+	{
+		if (get_option('facets_public_hook') == 'public_items_browse') {
+			self::showFacets($args);
+		}
+	}
+	
+	public function hookPublicItemsFacets($args)
+	{
+		if (get_option('facets_public_hook') == 'public_items_facets') {
+			self::showFacets($args);
+		}
+	}
+
+	public function showFacets($args)
+	{
+		$request = Zend_Controller_Front::getInstance()->getRequest();
+		$controller = $request->getControllerName();
+		$action = $request->getActionName();
+		
+		// checks whether it's one of the facet cases
+		if (!($controller == 'search' && $action == 'index') && !($controller == 'items' && $action == 'browse')) return;
+		
+		$itemsArray = array();
+		$params = array('advanced' => $_GET['advanced'], 'collection' => $_GET['collection'], 'type' => $_GET['type'], 'tags' => $_GET['tags']);
+		$items = get_records('item', $params, null);
+
+		if (count($items) > 0) {
+			if ($controller == 'items') {
+				foreach ($items as $item) {
+					$itemsArray[] = $item->id;
+				}
+				echo get_view()->partial('facets/browse.php', array(
+					'itemsArray' => $itemsArray
+				));
+			} elseif ($controller == 'search') {
+				// this would be for the site-wide simple search;
+				// main problem is that, differently from advanced search,
+				// it uses its own "search_texts" table, so results from that
+				// would have to be crossjoined with the "advanced" ones used
+				// by the facets;
+				// besides, advanced search works only for Items, so one might
+				// want to check whether the site-wide search has been performed
+				// just on Items.
+				if ($recordTypes = $_GET['record_types']) {
+					if (count($recordTypes) == 1 && in_array('Item', $recordTypes)) {
+						// site-wide search was performend just on Items
+					}
+				}
+			}
+		}
+	}
 }
---
+++ config_form.php
@@ -0,0 +1,153 @@
+<?php 
+	$view = get_view();
+?>
+<style type = "text/css">
+	.boxes {
+		text-align: center;
+		vertical-align: middle;
+	}
+</style>
+
+<h2><?php echo __('Use'); ?></h2>
+
+<div class="field">
+	<div class="two columns alpha">
+		<?php echo $view->formLabel('facets_public_hook', __('Public hook')); ?>
+	</div>
+	<div class="inputs five columns omega">
+		<p class="explanation">
+			<?php echo __('The hook you want to use: Omeka\'s default <code>public_items_browse</code>, already coded into every page (so no change is needed), normally showing up at the end of the page; or the plugin\'s own <code>public_items_facets</code>, that has to be coded into relevant theme pages but can be used wherever deemed right.'); ?>
+		</p>
+		<?php echo $view->formRadio('facets_public_hook',
+			get_option('facets_public_hook'),
+			null,
+			array(
+				'public_items_browse' => __('public_items_browse'),
+				'public_items_facets' => __('public_items_facets'),
+			)); 
+		?>
+    </div>
+</div>
+
+<h2><?php echo __('Aspect'); ?></h2>
+
+<div class="field">
+	<div class="two columns alpha">
+		<?php echo $view->formLabel('facets_description', __('Description')); ?>
+	</div>
+	<div class="inputs five columns omega">
+		<p class="explanation">
+			<?php echo __('Text to be used as additional description (tip: keep it short).'); ?>
+		</p>
+		<?php echo $view->formText('facets_description', get_option('facets_description')); ?>
+    </div>
+</div>
+
+<div class="field">
+	<div class="two columns alpha">
+		<?php echo $view->formLabel('facets_hide_single_entries', __('Hide single entries')); ?>
+	</div>
+	<div class="inputs five columns omega">
+		<p class="explanation">
+			<?php echo __('If checked, values with just one entry will not be listed (unless there\'s less than %s entries in the list).', FACETS_MINIMUM_AMOUNT); ?>
+		</p>
+		<?php echo $view->formCheckbox('facets_hide_single_entries', get_option('facets_hide_single_entries'), null, array('1', '0')); ?>
+    </div>
+</div>
+
+<div class="field">
+	<div class="two columns alpha">
+		<?php echo $view->formLabel('facets_sort_order', __('Sort order')); ?>
+	</div>
+	<div class="inputs five columns omega">
+		<p class="explanation">
+			<?php echo __('The sorting order for values in select fields.'); ?>
+		</p>
+		<?php echo $view->formRadio('facets_sort_order',
+			get_option('facets_sort_order'),
+			null,
+			array(
+				'alpha' => __('Alphabetical'),
+				'count_alpha' => __('Count first, then alphabetical'),
+			)); 
+		?>
+    </div>
+</div>
+
+<h2><?php echo __('Elements'); ?></h2>
+
+<div class="field">
+	<div class="two columns alpha">
+		<?php echo $view->formLabel('facets_elements-table', __('Dublin Core')); ?>
+	</div>
+	<div class="inputs five columns omega">
+		<p class="explanation">
+			<?php echo __('The Dublin Core elements that can be used for search refinement.'); ?>
+		</p>
+		<table id="facets_elements-table">
+			<thead>
+				<tr>
+					<th class="boxes"><?php echo __('Element'); ?></th>
+					<th class="boxes"><?php echo __('Active'); ?></th>
+				</tr>
+			</thead>
+			<tbody>
+			<?php
+				foreach ($elements as $element):
+			?>
+				<tr>
+					<td><?php echo __($element->name); ?></td>
+					<td class="boxes">
+						<?php echo $view->formCheckbox(
+							"item_elements[{$element->set_name}][{$element->name}]",
+							'1', 
+							array(
+								'disableHidden' => true,
+								'checked' => isset($settings['item_elements'][$element->set_name][$element->name])
+							)
+						); ?>
+					</td>
+				</tr>
+			<?php 
+				endforeach;
+			?>
+			</tbody>
+		</table>
+	</div>
+</div>
+
+<div class="field">
+	<div class="two columns alpha">
+		<?php echo $view->formLabel('facets_item_types', __('Item Types')); ?>
+	</div>
+	<div class="inputs five columns omega">
+		<p class="explanation">
+			<?php echo __('If checked, search refinement by Item Type will be available.'); ?>
+		</p>
+		<?php echo $view->formCheckbox('facets_item_types', get_option('facets_item_types'), null, array('1', '0')); ?>
+    </div>
+</div>
+
+<div class="field">
+	<div class="two columns alpha">
+		<?php echo $view->formLabel('facets_collections', __('Collections')); ?>
+	</div>
+	<div class="inputs five columns omega">
+		<p class="explanation">
+			<?php echo __('If checked, search refinement by Collection will be available.'); ?>
+		</p>
+		<?php echo $view->formCheckbox('facets_collections', get_option('facets_collections'), null, array('1', '0')); ?>
+    </div>
+</div>
+
+<div class="field">
+	<div class="two columns alpha">
+		<?php echo $view->formLabel('facets_tags', __('Tags')); ?>
+	</div>
+	<div class="inputs five columns omega">
+		<p class="explanation">
+			<?php echo __('If checked, search refinement by Tags will be available.'); ?>
+		</p>
+		<?php echo $view->formCheckbox('facets_tags', get_option('facets_tags'), null, array('1', '0')); ?>
+    </div>
+</div>
