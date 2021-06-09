e49aed0d15f5b9af2c509e49eeade3fb73af2af5
DBinaghi <39097246+DBinaghi@users.noreply.github.com>
2021-06-09 14:54:37 +0200

    Extended feature to Collections

--- FacetsPlugin.php
+++ FacetsPlugin.php
@@ -9,7 +9,7 @@
 if (!defined('FACETS_PLUGIN_DIR')) {
 	define('FACETS_PLUGIN_DIR', dirname(__FILE__));
 }
-define('FACETS_MINIMUM_AMOUNT', 10);
+define('FACETS_MINIMUM_AMOUNT', 5);
 
 require_once FACETS_PLUGIN_DIR . '/helpers/FacetsFunctions.php';
 
@@ -22,9 +22,11 @@ class FacetsPlugin extends Omeka_Plugin_AbstractPlugin
 		'config_form',
 		'config',
 		'define_routes',
+        'collections_browse_sql',
 		'public_head',
 		'public_items_browse',
-		'public_items_facets'
+		'public_collections_browse',
+		'public_facets'
 	);
 
 	/**
@@ -32,14 +34,15 @@ class FacetsPlugin extends Omeka_Plugin_AbstractPlugin
 	 */
 	public function hookInstall()
 	{
-		set_option('facets_public_hook', 'public_items_browse');
+		set_option('facets_public_hook', 'default');
 		set_option('facets_description', '');
 		set_option('facets_hide_single_entries', 0);
 		set_option('facets_sort_order', 'count_alpha');
 		set_option('facets_hide_popularity', 0);
 
 		$defaults = array(
-			'item_elements' => array()
+			'item_elements' => array(),
+			'collection_elements' => array()
 		);
 		set_option('facets_elements', json_encode($defaults));
 		set_option('facets_item_types', 1);
@@ -68,7 +71,7 @@ class FacetsPlugin extends Omeka_Plugin_AbstractPlugin
 	 */
 	public function hookInitialize()
 	{
-		add_translation_source(dirname(__FILE__) . '/languages');
+        add_translation_source(dirname(__FILE__) . '/languages');
 
 		get_view()->addHelperPath(dirname(__FILE__) . '/views/helpers', 'Facets_View_Helper_');
 
@@ -108,18 +111,14 @@ class FacetsPlugin extends Omeka_Plugin_AbstractPlugin
 		set_option('facets_hide_popularity', $post['facets_hide_popularity']);
 
 		$settings = array(
-			'item_elements' => isset($post['item_elements']) ? $post['item_elements'] : array()
+			'item_elements' => isset($post['item_elements']) ? $post['item_elements'] : array(),
+			'collection_elements' => isset($post['collection_elements']) ? $post['collection_elements'] : array()
 		);
 		set_option('facets_elements', json_encode($settings));
 
 		set_option('facets_item_types', $post['facets_item_types']);
 		set_option('facets_collections', $post['facets_collections']);
 		set_option('facets_tags', $post['facets_tags']);
-	}
-
-	public function hookPublicHead($args)
-	{
-		queue_css_file('facets');
 	}
 
 	public function hookDefineRoutes($args)
@@ -143,26 +142,150 @@ class FacetsPlugin extends Omeka_Plugin_AbstractPlugin
 		);
 	}
 
+    /**
+     * Hook into collections_browse_sql
+     *
+     * @select array $args
+     * @params array $args
+     */
+    public function hookCollectionsBrowseSql($args)
+    {
+        $db = $this->_db;
+        $select = $args['select'];
+        $params = $args['params'];
+		
+		if (strpos($select, '_advanced') !== false) return;
+        
+        if ($advancedTerms = @$params['advanced']) {
+            $where = '';
+            $advancedIndex = 0;
+            foreach ($advancedTerms as $v) {
+                // Do not search on blank rows.
+                if (empty($v['element_id']) || empty($v['type'])) {
+                    continue;
+                }
+
+                $value = isset($v['terms']) ? $v['terms'] : null;
+                $type = $v['type'];
+                $elementId = (int) $v['element_id'];
+                $alias = "_advanced_{$advancedIndex}";
+
+                $joiner = isset($v['joiner']) && $advancedIndex > 0 ? $v['joiner'] : null;
+
+                $negate = false;
+                // Determine what the WHERE clause should look like.
+                switch ($type) {
+                    case 'does not contain':
+                        $negate = true;
+                    case 'contains':
+                        $predicate = "LIKE " . $db->quote('%'.$value .'%');
+                        break;
+
+                    case 'is not exactly':
+                        $negate = true;
+                    case 'is exactly':
+                        $predicate = ' = ' . $db->quote($value);
+                        break;
+
+                    case 'is empty':
+                        $negate = true;
+                    case 'is not empty':
+                        $predicate = 'IS NOT NULL';
+                        break;
+
+                    case 'starts with':
+                        $predicate = "LIKE " . $db->quote($value.'%');
+                        break;
+
+                    case 'ends with':
+                        $predicate = "LIKE " . $db->quote('%'.$value);
+                        break;
+
+                    case 'does not match':
+                        $negate = true;
+                    case 'matches':
+                        if (!strlen($value)) {
+                            continue 2;
+                        }
+                        $predicate = 'REGEXP ' . $db->quote($value);
+                        break;
+
+                    default:
+                        throw new Omeka_Record_Exception(__('Invalid search type given!'));
+                }
+
+                $predicateClause = "{$alias}.text {$predicate}";
+
+                // Note that $elementId was earlier forced to int, so manual quoting
+                // is unnecessary here
+                $joinCondition = "{$alias}.record_id = collections.id AND {$alias}.record_type = 'Collection' AND {$alias}.element_id = $elementId";
+
+                if ($negate) {
+                    $joinCondition .= " AND {$predicateClause}";
+                    $whereClause = "{$alias}.text IS NULL";
+                } else {
+                    $whereClause = $predicateClause;
+                }
+
+                $select->joinLeft(array($alias => $db->ElementText), $joinCondition, array());
+                if ($where == '') {
+                    $where = $whereClause;
+                } elseif ($joiner == 'or') {
+                    $where .= " OR $whereClause";
+                } else {
+                    $where .= " AND $whereClause";
+                }
+
+                $advancedIndex++;
+            }
+
+            if ($where) {
+                $select->where($where);
+            }
+        }            
+    }
+
+	public function hookPublicHead($args)
+	{
+		$request = Zend_Controller_Front::getInstance()->getRequest();
+		$controller = $request->getControllerName();
+		$action = $request->getActionName();
+ 		
+		if (($action == 'browse' && ($controller == 'items' || $controller == 'collections')) || ($controller == 'search' && $action == 'index')) {
+			queue_css_file('facets');
+		}
+	}
+
 	public function hookPublicItemsBrowse($args)
 	{
-		if (get_option('facets_public_hook') == 'public_items_browse') {
+		$settings = $this->_settings;
+		if (get_option('facets_public_hook') == 'default') {
 			self::showFacets($args);
 		}
 	}
 	
-	public function hookPublicItemsFacets($args)
+	public function hookPublicCollectionsBrowse($args)
 	{
-		if (get_option('facets_public_hook') == 'public_items_facets') {
+		$settings = $this->_settings;
+		if (get_option('facets_public_hook') == 'default') {
+			self::showFacets($args);
+		}
+	}
+	
+	public function hookPublicFacets($args)
+	{
+		if (get_option('facets_public_hook') == 'custom') {
 			self::showFacets($args);
 		}
 	}
 
 	public function showFacets($args)
 	{
+		$settings = $this->_settings;
 		$request = Zend_Controller_Front::getInstance()->getRequest();
 		$controller = $request->getControllerName();
 		$action = $request->getActionName();
-		
+ 		
 		if ($controller == 'items' && $action == 'browse') {
 			$params = array(
 				'advanced' => $_GET['advanced'], 
@@ -170,10 +293,20 @@ class FacetsPlugin extends Omeka_Plugin_AbstractPlugin
 				'type' => $_GET['type'], 
 				'tags' => $_GET['tags']
 			);
-			if (count(get_records('item', $params, null)) > 0) {
+			if (count($settings['item_elements']) > 0 && count(get_records('item', $params, null)) > 0) {
 				echo get_view()->partial('facets/browse.php', array(
-					'itemsArray' => $itemsArray,
-					'params' => $params
+					'params' => $params,
+					'recordType' => 'item'
+				));
+			}
+		} elseif ($controller == 'collections' && $action == 'browse') {
+			$params = array(
+				'advanced' => $_GET['advanced']
+			);
+			if (count($settings['collection_elements']) > 0 && count(get_records('collection', $params, null)) > 0) {
+				echo get_view()->partial('facets/browse.php', array(
+					'params' => $params,
+					'recordType' => 'collection'
 				));
 			}
 		} elseif ($controller == 'search' && $action == 'index') {
