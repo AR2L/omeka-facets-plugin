f88eceb838c40ff9b60e1bf415bf2213c491fbe3
DBinaghi <39097246+DBinaghi@users.noreply.github.com>
2021-07-17 18:19:16 +0200

    Added checkbox style

--- views/public/facets/browse.php
+++ views/public/facets/browse.php
@@ -3,6 +3,7 @@
 	$hideSingleEntries = (bool)get_option('facets_hide_single_entries');
 	$facetsCollapsible = (bool)get_option('facets_collapsible');
 	$facetsDirection = (string)get_option('facets_direction');
+	$checkboxMinCount = (int)get_option('facets_checkbox_minimum_amount');
 
 	$table = get_db()->getTable('Element');
 	$select = $table->getSelect()
@@ -18,7 +19,7 @@
 	}
 ?>
 
-<div class="facets-container <?php echo "facets-layout-" . $facetsDirection; ?>">
+<div id="facets-container" class="<?php echo "facets-layout-" . $facetsDirection; ?>">
 	<button id="facets-title" <?php if ($facetsCollapsible) echo "class=\"facets-collapsible\""; ?>><?php echo html_escape(__('Refine search')) ?></button>
 	<div id="facets-body">
 		<?php 
@@ -34,10 +35,10 @@
 						if (isFacetActive($recordType, $element->name, $facetsElements)) {
 							$isDate = in_array($element->name, array('Date'));
 							$facetElement = $facetsElements['elements'][$element->name];
-							if ($html = get_facet_select('collection', $subsetSQL, $element->id, $isDate, $hideSingleEntries, (isset($facetElement['sort']) ? $facetElement['sort'] : ''), (isset($facetElement['popularity']) ? $facetElement['popularity'] : ''))) {
-								echo "<div class=\"container-" . $facetsDirection . "\">\n";
+							if ($html = get_element_facet_select('collection', $subsetSQL, $element->id, $isDate, $hideSingleEntries, (isset($facetElement['sort']) ? $facetElement['sort'] : ''), (isset($facetElement['popularity']) ? $facetElement['popularity'] : ''))) {
+								echo "<div id=\"facets-field-" . $element->id . "\" class=\"facets-container-" . $facetsDirection . "\">\n";
 								echo "<label for=\"\">" . html_escape(__($element->name)) . "</label>\n";
-								echo $html;
+								echo $html . "\n";
 								echo "</div>\n";
 							}
 						}
@@ -47,38 +48,43 @@
 						if (isFacetActive($recordType, $element->name, $facetsElements)) {
 							$isDate = in_array($element->name, array('Date'));
 							$facetElement = $facetsElements['elements'][$element->name];
-							if ($html = get_facet_select('item', $subsetSQL, $element->id, $isDate, $hideSingleEntries, (isset($facetElement['sort']) ? $facetElement['sort'] : ''), (isset($facetElement['popularity']) ? $facetElement['popularity'] : ''))) {
-								echo "<div class=\"container-" . $facetsDirection . "\">\n";
-								echo "<label for=\"\">" . html_escape(__($element->name)) . "</label>\n";
-								echo $html;
-								echo "</div>\n";
+							if (isset($facetElement['type']) && $facetElement['type'] == 'checkbox') {
+								$html = get_element_facet_checkboxes('item', $subsetSQL, $element->id, $isDate, $hideSingleEntries, (isset($facetElement['sort']) ? $facetElement['sort'] : ''), (isset($facetElement['popularity']) ? $facetElement['popularity'] : ''), $checkboxMinCount);
+							} else {
+								$html = get_element_facet_select('item', $subsetSQL, $element->id, $isDate, $hideSingleEntries, (isset($facetElement['sort']) ? $facetElement['sort'] : ''), (isset($facetElement['popularity']) ? $facetElement['popularity'] : ''));
 							}
+							if ($html != '') {
+								echo "<div id=\"facets-field-" . $element->id . "\" class=\"facets-container-" . $facetsDirection . "\">\n";
+								echo "<label for=\"\">" . html_escape(__($element->name)) . "</label>\n";
+								echo $html . "\n";
+								echo "</div>\n";
+							}								
 						}
 					}
 
 					if ((bool)get_option('facets_item_types_active')) {
 						if ($html = get_item_types_facet_select($subsetSQL, $hideSingleEntries, get_option('facets_item_types_sort'), get_option('facets_item_types_popularity'))) {
-							echo "<div class=\"container-" . $facetsDirection . "\">\n";
+							echo "<div id=\"facets-field-item-type\" class=\"facets-container-" . $facetsDirection . "\">\n";
 							echo "<label for=\"\">" . html_escape(__('Item Type')) . "</label>\n";
-							echo $html;
+							echo $html . "\n";
 							echo "</div>\n";
 						}
 					}
 
 					if ((bool)get_option('facets_collections_active')) {
 						if ($html = get_collections_facet_select($subsetSQL, $hideSingleEntries, get_option('facets_collections_sort'), get_option('facets_collections_popularity'))) {
-							echo "<div class=\"container-" . $facetsDirection . "\">\n";
+							echo "<div id=\"facets-field-collection\" class=\"facets-container-" . $facetsDirection . "\">\n";
 							echo "<label for=\"\">" . html_escape(__('Collection')) . "</label>\n";
-							echo $html;
+							echo $html . "\n";
 							echo "</div>\n";
 						}
 					}
 
 					if ((bool)get_option('facets_tags_active')) {
 						if ($html = get_tags_facet_select($subsetSQL, $hideSingleEntries, get_option('facets_tags_sort'), get_option('facets_tags_popularity'))) {
-							echo "<div class=\"container-" . $facetsDirection . "\">\n";
+							echo "<div id=\"facets-field-tag\" class=\"facets-container-" . $facetsDirection . "\">\n";
 							echo "<label for=\"\">" . html_escape(__('Tags')) . "</label>\n";
-							echo $html;
+							echo $html . "\n";
 							echo "</div>\n";
 						}
 					}
@@ -87,34 +93,3 @@
 		</form>
 	</div>
 </div>
-<script type="text/javascript">
-	// force block collapsible if screen size small
-	jQuery(window).on('load', function() {
-		if (jQuery(window).width() < 768) {
-			jQuery('#facets-title').addClass('facets-collapsed');
-			jQuery('#facets-body').addClass('hidden');
-		}
-	});
-	// hides facets block (but for title) on load
-	jQuery('#facets-body').next().addClass('hidden');
-	// submit results of refining search to reload the page
-	window.jQuery(document).ready(function() {
-		window.jQuery('select').change(function() {
-			var option = window.jQuery(this).find('option:selected');
-			if (typeof(option.data('url')) !== 'undefined') window.location.href = option.data('url');
-		});
-	});
-	// collapse/expands facets block
-	jQuery('#facets-title').click(function () {
-		$header = jQuery(this);
-		if (!$header.hasClass('facets-collapsible') && !$header.hasClass('facets-collapsed')) return;
-		//getting the next element
-		$content = $header.next();
-		//open up the content needed - toggle the slide- if visible, slide up, if not slidedown.
-		$content.slideToggle(300, function () {
-			//execute this after slideToggle is done
-			//change icon of header based on visibility of content div
-			$header.toggleClass('facets-collapsible facets-collapsed');
-		});
-	});
-</script>
